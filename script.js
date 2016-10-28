8f6348075ba9ab3b36153f8ea9098739046c50bd
Pomerani <mireille.lanser@hotmail.com>
2016-10-28 13:02:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//var div =
+//    document.querySelector ('.likes');
+//
+// document.getElementsByClassName("likes").src = "images/greenheart-01.png"
+//
+//var heartfill = function () {
+//    div.classList.toggle('fill')
+//}
+//
+//div.addEventListener('click', heartfill);
+
+//function changeimg () {
+//    var emptyheart = document.getElementsByClassName("likes")[0];
+//    
+//    if(likes.src == "/images/greenheart-01.png"){
+//        likes.src = "/images/heart-01.png";
+//    }
+//    else {
+//        likes.src = "/images/heart-01.png";
+//    }
+//}
+
+//var div = document.querySelectorAll(".harmonica");
+//for (var i = 0; i < div.length; i++) {
+//    div[i].addEventListener("click", function () {
+//        this.classList.toggle("uitklappen");
+//    });
+//}
+
+//var div =  document.querySelectorAll('.harmonica');
+//for(var i = 0; i < div.length; i++) {
+//div[i].addEventListener('click', function() {
+//this.classList.toggle('uitklapppen');
+//});
+//}
